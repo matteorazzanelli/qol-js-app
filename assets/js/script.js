444172ad89cdaee6308a36e613826cce4cd83f06
matteo <matteo.razzanelli89@gmail.com>
2023-01-06 19:23:48 +0100

    some preliminary tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 
 
-// let prova = fetch("https://api.teleport.org/api/cities/?search=san%20francisco");
-// let response = await prova.json();
-// let request = await fetch('https://api.teleport.org/api/');
-// let response = request.json();
+
 
 async function getAllBooks(param){
   // let request = await fetch(`https://openlibrary.org/subjects/${param}.json`);
-  let request = await fetch(`https://api.teleport.org/api/cities/?search=san%20francisco`);
+  let request = await fetch(`https://api.teleport.org/api/cities/?search=${param}`);
   let response = await request.json();
 
   return response;
 }
 
-getAllBooks('san%20francisco').then(response => {
+getAllBooks('san francisco').then(response => {
+  console.log(response._embedded["city:search-results"][0]["_links"]["city:item"]);
+}).catch(e =>{
+  console.log('ERROR');
+});
+
+
+async function getAllBooks2(param){
+  let request = await fetch(`https://api.teleport.org/api/urban_areas/slug:${param}/scores/`);
+  let response = await request.json();
+
+  return response;
+}
+
+getAllBooks2('san-francisco-bay-area').then(response => {
   console.log(response);
 }).catch(e =>{
   console.log('ERROR');
