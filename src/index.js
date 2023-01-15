51fbad9ce5c21f235d6336671137ae8fd842af27
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2023-01-15 18:02:28 +0100

    minor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'; //"firebase/app"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"; //"firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
